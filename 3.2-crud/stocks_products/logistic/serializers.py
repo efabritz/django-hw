eaf06113f33081c9906dbb38ee7bb11040def3fc
efabritz <katya-belova@inbox.ru>
2022-11-21 23:15:58 +0100

    stocks_products

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
 
+from logistic.models import Product, Stock, StockProduct
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['id', 'product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -23,9 +29,8 @@ class StockSerializer(serializers.ModelSerializer):
         # создаем склад по его параметрам
         stock = super().create(validated_data)
 
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(stock = stock, product = position['product'], quantity = position['quantity'], price = position['price'])
 
         return stock
 
@@ -36,8 +41,8 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(stock=stock, product=position['product'], quantity=position['quantity'],
+                                        price=position['price'])
 
         return stock
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,10 +9,47 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['^products__title', '^products__description']
+    filterset_fields = ['products']
+
+# 1
+# DELETE {{baseUrl}}/stocks/1
+# Content-Type: application/json
+# HTTP/1.1 301 Moved Permanently
+# Date: Mon, 21 Nov 2022 13:35:43 GMT
+# Server: WSGIServer/0.2 CPython/3.8.10
+# Content-Type: text/html; charset=utf-8
+# Location: /api/v1/stocks/1/
+# Content-Length: 0
+# X-Content-Type-Options: nosniff
+# Referrer-Policy: same-origin
+# Cross-Origin-Opener-Policy: same-origin
+
+# 2
+# обновляем записи на складе ---
+# PATCH {{baseUrl}}/stocks/4/
+# Content-Type: application/json
+#
+# {
+#   "positions": [
+#     {
+#       "product": 2,
+#       "quantity": 100,
+#       "price": 130.80
+#     },
+#     {
+#       "product": 3,
+#       "quantity": 243,
+#       "price": 145
+#     }
+#   ]
+# }
+# 2 подряд делает list concatination вместо обновления
